14af9723363bc31745d289806f3add3b5dbdc53a
Raymond Feng <raymond@strongloop.com>
2013-06-25 09:29:53 -0700

    Add Container/File classes

--- app.js
+++ app.js
@@ -2,21 +2,45 @@ var storage = require('pkgcloud').storage;
 
 var client = storage.createClient({
     provider: 'rackspace',
-    username: 'your-user-name',
-    apiKey: 'your-api-key'
-  });
+    username: 'strongloop',
+    apiKey: 'your-rackspace-api-key'
+});
 
 // Container
 
-client.getContainers(function (err, containers) { });
-client.createContainer(options, function (err, container) { });
-client.destroyContainer(containerName, function (err) { });
-client.getContainer(containerName, function (err, container) { });
+client.getContainers(function (err, containers) {
+    containers.forEach(function(c) {
+       console.log(c.name);
+    });
+});
 
-// File
+/*
+ client.createContainer(options, function (err, container) { });
+ client.destroyContainer(containerName, function (err) { });
+ client.getContainer(containerName, function (err, container) { });
 
-client.upload(options, function (err) { });
-client.download(options, function (err) { });
-client.getFiles(container, function (err, files) { });
-client.getFile(container, file, function (err, server) { });
-client.removeFile(container, file, function (err) { });
+ // File
+
+ client.upload(options, function (err) { });
+ client.download(options, function (err) { });
+ client.getFiles(container, function (err, files) { });
+ client.getFile(container, file, function (err, server) { });
+ client.removeFile(container, file, function (err) { });
+ */
+
+
+var s3 = storage.createClient({
+    provider: 'amazon',
+    key: 'your-amazon-key',
+    keyId: 'your-amazon-key-id'
+});
+
+s3.getContainers(function (err, containers) {
+    if(err) {
+        console.error(err);
+        return;
+    }
+    containers.forEach(function(c) {
+        console.log(c.name);
+    });
+});
