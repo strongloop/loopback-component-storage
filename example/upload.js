427f270a40c5c42605fe143dc24d4323a421cf81
Raymond Feng <raymond@strongloop.com>
2013-06-27 13:50:57 -0700

    Support uploading multiple files

--- example/upload.js
+++ example/upload.js
@@ -21,7 +21,7 @@ app.get('/', function (req, res, next) {
         "<a href='/download'>List all containers</a><p>" +
         "Upload to container c1: <p>" +
         "<form method='POST' enctype='multipart/form-data' action='/upload/c1'>"
-        + "File to upload: <input type=file name=upfile><br>"
+        + "File to upload: <input type=file name=uploadedFiles multiple=true><br>"
         + "Notes about the file: <input type=text name=note><br>"
         + "<input type=submit value=Upload></form>" +
         "</body></html>";
