18b27ff320d14b6c7d5d07f8b2ec76a986a6dfdf
Raymond Feng <raymond@strongloop.com>
2013-07-24 12:07:46 -0700

    Fix for https://strongloop.atlassian.net/browse/SLA-316

--- test/storage-service.test.js
+++ test/storage-service.test.js
@@ -1,29 +1,16 @@
-var FileSystemProvider = require('../lib/providers/filesystem/index.js').Client;
+var StorageService = require('../lib/index.js');
 
 var assert = require('assert');
 var path = require('path');
 
-describe('FileSystem based storage provider', function () {
+var storageService = new StorageService({root: path.join(__dirname, 'storage'), provider: 'filesystem'});
+
+describe('Storage service', function () {
 
     describe('container apis', function () {
-        var client = null;
-        it('should require an existing directory as the root', function (done) {
-            client = new FileSystemProvider({root: path.join(__dirname, 'storage')});
-            process.nextTick(done);
-        });
-
-        it('should complain if the root directory doesn\'t exist', function (done) {
-            try {
-                client = new FileSystemProvider({root: path.join(__dirname, '_storage')});
-                process.nextTick(done.bind(null, 'Error'));
-            } catch (err) {
-                // Should be here
-                process.nextTick(done);
-            }
-        });
 
         it('should return an empty list of containers', function (done) {
-            client.getContainers(function (err, containers) {
+            storageService.getContainers(function (err, containers) {
                 assert(!err);
                 assert.equal(0, containers.length);
                 done(err, containers);
@@ -31,28 +18,28 @@ describe('FileSystem based storage provider', function () {
         });
 
         it('should create a new container', function (done) {
-            client.createContainer({name: 'c1'}, function (err, container) {
+            storageService.createContainer({name: 'c1'}, function (err, container) {
                 assert(!err);
                 done(err, container);
             });
         });
 
         it('should get a container c1', function (done) {
-            client.getContainer('c1', function (err, container) {
+            storageService.getContainer('c1', function (err, container) {
                 assert(!err);
                 done(err, container);
             });
         });
 
         it('should not get a container c2', function (done) {
-            client.getContainer('c2', function (err, container) {
+            storageService.getContainer('c2', function (err, container) {
                 assert(err);
                 done(null, container);
             });
         });
 
         it('should return one container', function (done) {
-            client.getContainers(function (err, containers) {
+            storageService.getContainers(function (err, containers) {
                 assert(!err);
                 assert.equal(1, containers.length);
                 done(err, containers);
@@ -60,14 +47,14 @@ describe('FileSystem based storage provider', function () {
         });
 
         it('should destroy a container c1', function (done) {
-            client.destroyContainer('c1', function (err, container) {
+            storageService.destroyContainer('c1', function (err, container) {
                 assert(!err);
                 done(err, container);
             });
         });
 
         it('should not get a container c1 after destroy', function (done) {
-            client.getContainer('c1', function (err, container) {
+            storageService.getContainer('c1', function (err, container) {
                 assert(err);
                 done(null, container);
             });
@@ -76,34 +63,30 @@ describe('FileSystem based storage provider', function () {
 
     describe('file apis', function () {
         var fs = require('fs');
-        var client = new FileSystemProvider({root: path.join(__dirname, 'storage')});
 
         it('should create a new container', function (done) {
-            client.createContainer({name: 'c1'}, function (err, container) {
+            storageService.createContainer({name: 'c1'}, function (err, container) {
                 assert(!err);
                 done(err, container);
             });
         });
 
         it('should upload a file', function (done) {
-            var writer = client.upload({container: 'c1', remote: 'f1.txt'});
+            var writer = storageService.uploadStream('c1', 'f1.txt');
             fs.createReadStream(path.join(__dirname, 'files/f1.txt')).pipe(writer);
             writer.on('finish', done);
             writer.on('error', done);
         });
 
         it('should download a file', function (done) {
-            var reader = client.download({
-                container: 'c1',
-                remote: 'f1.txt'
-            });
+            var reader = storageService.downloadStream('c1','f1.txt');
             reader.pipe(fs.createWriteStream(path.join(__dirname, 'files/f1_downloaded.txt')));
             reader.on('end', done);
             reader.on('error', done);
         });
 
         it('should get files for a container', function (done) {
-            client.getFiles('c1', function (err, files) {
+            storageService.getFiles('c1', function (err, files) {
                 assert(!err);
                 assert.equal(1, files.length);
                 done(err, files);
@@ -111,7 +94,7 @@ describe('FileSystem based storage provider', function () {
         });
 
         it('should get a file', function (done) {
-            client.getFile('c1', 'f1.txt', function (err, f) {
+            storageService.getFile('c1', 'f1.txt', function (err, f) {
                 assert(!err);
                 assert.ok(f);
                 done(err, f);
@@ -119,14 +102,14 @@ describe('FileSystem based storage provider', function () {
         });
 
         it('should remove a file', function (done) {
-            client.removeFile('c1', 'f1.txt', function (err) {
+            storageService.removeFile('c1', 'f1.txt', function (err) {
                 assert(!err);
                 done(err);
             });
         });
 
         it('should get no files from a container', function (done) {
-            client.getFiles('c1', function (err, files) {
+            storageService.getFiles('c1', function (err, files) {
                 assert(!err);
                 assert.equal(0, files.length);
                 done(err, files);
@@ -134,7 +117,7 @@ describe('FileSystem based storage provider', function () {
         });
 
         it('should destroy a container c1', function (done) {
-            client.destroyContainer('c1', function (err, container) {
+            storageService.destroyContainer('c1', function (err, container) {
                 // console.error(err);
                 assert(!err);
                 done(err, container);
