0f9407908202b65dd3924a92948fa3b241136eff
Raymond Feng <raymond@strongloop.com>
2014-01-14 10:39:02 -0800

    Fix the ref to StorageService

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var StorageConnector = require('./lib/storage-connector');
 StorageConnector.Container = require('./lib/models/container');
 StorageConnector.File = require('./lib/models/file');
+StorageConnector.StorageService = require('./lib/storage-service');
 
 module.exports = StorageConnector;
 
