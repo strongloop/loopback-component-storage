af2a7adfb42acaee35b372424d10a721a3721a5a
Raymond Feng <raymond@strongloop.com>
2014-03-31 17:33:32 -0700

    Clean up the sample

--- example/app-cloud.js
+++ example/app-cloud.js
@@ -7,78 +7,51 @@ try {
   providers = require('./providers.json');
 }
 
-var rs = StorageService({
+function listContainersAndFiles(ss) {
+  ss.getContainers(function (err, containers) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('----------- %s (%d) ---------------', ss.provider, containers.length);
+    containers.forEach(function (c) {
+      console.log('[%s] %s/', ss.provider, c.name);
+      c.getFiles(function (err, files) {
+        files.forEach(function (f) {
+          console.log('[%s] ... %s', ss.provider, f.name);
+        });
+      });
+    });
+  });
+}
+
+var rs = new StorageService({
   provider: 'rackspace',
   username: providers.rackspace.username,
   apiKey: providers.rackspace.apiKey,
   region: providers.rackspace.region
 });
 
-// Container
+listContainersAndFiles(rs);
 
-rs.getContainers(function (err, containers) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  containers.forEach(function (c) {
-    console.log('rackspace: ', c.toJSON());
-    c.getFiles(function (err, files) {
-      files.forEach(function (f) {
-        console.log('....', f.toJSON());
-      });
-    });
-  });
-});
-
-/*
-var s3 = StorageService({
+var s3 = new StorageService({
   provider: 'amazon',
   key: providers.amazon.key,
   keyId: providers.amazon.keyId
 });
 
-s3.getContainers(function (err, containers) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  containers.forEach(function (c) {
-    console.log('amazon: ', c.name);
-    c.getFiles(function (err, files) {
-      files.forEach(function (f) {
-        console.log('....', f.name);
-      });
-    });
-  });
-});
+listContainersAndFiles(s3);
 
 
 var fs = require('fs');
 var path = require('path');
 var stream = s3.uploadStream('con1', 'test.jpg');
 fs.createReadStream(path.join(__dirname, 'test.jpg')).pipe(stream);
-*/
 
 var local = StorageService({
   provider: 'filesystem',
   root: path.join(__dirname, 'storage')
 });
 
-// Container
-
-local.getContainers(function (err, containers) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  containers.forEach(function (c) {
-    console.log('filesystem: ', c.name);
-    c.getFiles(function (err, files) {
-      files.forEach(function (f) {
-        console.log('....', f.name);
-      });
-    });
-  });
-});
+listContainersAndFiles(local);
 
