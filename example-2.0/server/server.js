78c3b06cbe24ccdf49800ec17d0883d95399a766
crandmck <rand@strongloop.com>
2015-09-29 15:54:13 -0700

    Upgrade LB Explorer and related changes

--- example-2.0/server/server.js
+++ example-2.0/server/server.js
@@ -3,18 +3,25 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
-// boot scripts mount components like REST API
-boot(app, __dirname);
-
 app.start = function() {
   // start the web server
   return app.listen(function() {
     app.emit('started');
-    console.log('Web server listening at: %s', app.get('url'));
+    var baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
   });
 };
 
-// start the server if `$ node server.js`
-if (require.main === module) {
-  app.start();
-}
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+
+  // start the server if `$ node server.js`
+  if (require.main === module)
+    app.start();
+});
